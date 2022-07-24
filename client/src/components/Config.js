4b8570761b47fd318503c078e35b38c4ef6c5045
tjdvyzl <tjdvyzl12@naver.com>
2022-07-24 10:15:05 +0000

    Load More Button

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '9c6b011e6b5f822d69d4addfe156457f';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
